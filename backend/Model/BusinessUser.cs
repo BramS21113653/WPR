6f323c8aeadd9b979c4f2b22d38a1979e846d66c
Bram Steenkamp <bramsteenkamp@brams-air.wireless.hhs.nl>
2023-11-27 11:57:15 +0100

    init

---
+++ backend/Model/BusinessUser.cs
@@ -0,0 +1,7 @@
+public class BusinessUser : ApplicationUser
+{
+    public string CompanyName { get; set; }
+    public string CompanyLocation { get; set; }
+    public string CompanyWebsiteLink { get; set; }
+
+}
