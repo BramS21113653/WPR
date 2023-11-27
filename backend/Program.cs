17183eb6f55849009ffcf15b16a470ea7ab09fcc
Remcolangemaire <47173702+Remcolangemaire@users.noreply.github.com>
2023-11-27 14:42:05 +0100

    idk 2

--- backend/Program.cs
+++ backend/Program.cs
@@ -11,6 +11,13 @@ builder.Services.AddIdentity<IdentityUser, IdentityRole>()
                 .AddDefaultTokenProviders();
 builder.Services.AddAuthentication();
 // Add services to the container.
+builder.Services.AddCors(options =>
+    {
+        options.AddPolicy("AllowSpecificOrigin",
+            builder => builder.WithOrigins("http://localhost:3000") // Vervang met je Vite server URL
+                               .AllowAnyHeader()
+                               .AllowAnyMethod());
+    });
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -25,6 +32,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("AllowSpecificOrigin");
 
 app.UseHttpsRedirection();
 app.UseAuthentication();
