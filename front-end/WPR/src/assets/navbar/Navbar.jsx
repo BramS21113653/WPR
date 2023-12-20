1a15dcdcfa0eb229b46740ce510f8154bd4be17c
Remcolangemaire <Remcolangemaire@gmail.com>
2023-12-20 20:02:27 +0100

    nav2

--- front-end/WPR/src/assets/navbar/Navbar.jsx
+++ front-end/WPR/src/assets/navbar/Navbar.jsx
@@ -1,29 +1,42 @@
-import React from "react";
-import logo from '../LOGO.png'; // Adjusted path going one directory up then to assets
-import './Navbar.scss';
+// Navbar.jsx
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+import logo from '../LOGO.png'; // Adjust the path as needed
+import './Navbar.scss';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
     return (
         <nav>
             <div className="logo">
-                <a href="/">
+                <Link to="/">
                     <img src={logo} alt="Logo" />
-                </a>
+                </Link>
             </div>
 
-            <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/over-ons">Over Ons</Link></li>
-            <li><Link to="/nieuws">Nieuws</Link></li>
-            <li><Link to="/contact">Contact</Link></li>
-            <li><Link to="/portal">Portal</Link></li>
-            <li><Link to="/onderzoeken">Onderzoeken</Link></li>
-            <li><Link to="/inloggen">Inloggen</Link></li>
-            <li><Link to="/registreren">Registreren</Link></li>
+            <button className="hamburger" onClick={toggleMenu}>
+                {/* Icon from Font Awesome or similar library */}
+                <span className="hamburger-lines"></span>
+            </button>
+
+            <ul className={isOpen ? "nav-links open" : "nav-links"}>
+                <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
+                <li><Link to="/over-ons" onClick={toggleMenu}>Over Ons</Link></li>
+                <li><Link to="/nieuws" onClick={toggleMenu}>Nieuws</Link></li>
+                <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
+                <li><Link to="/portal" onClick={toggleMenu}>portal</Link></li>
+                <li><Link to="/onderzoeke" onClick={toggleMenu}>onderzoeke</Link></li>
+                <li><Link to="/inloggen" onClick={toggleMenu}>inloggen</Link></li>
+                <li><Link to="/registreren" onClick={toggleMenu}>registreren</Link></li>
             </ul>
         </nav>
     );
 };
 
 export default Navbar;
+
