9b6893dd2ecd914153679d13a8fdb890b641ef32
BramS21113653 <21113653@student.hhs.nl>
2023-12-20 19:22:46 +0100

    front-end

--- front-end/WPR/src/Contact.jsx
+++ front-end/WPR/src/Contact.jsx
@@ -3,9 +3,16 @@ import React from 'react';
 const Contact = () => {
   return (
     <div className="main-content">
-      <h1>Contact</h1>
-      <p></p>  
-    </div>
+        <h2>Contactgegevens</h2>
+        <p>
+          <strong>E-mail:</strong> 
+          <a href="mailto:info@accessibility.nl">info@accessibility.nl</a>
+        </p>
+        <p>
+          <strong>Telefoonnummer:</strong> 
+          <a href="tel:030-2398270">030-2398270</a>
+        </p>
+      </div>
   );
 };
 
--- front-end/WPR/src/assets/footer/Footer.jsx
+++ front-end/WPR/src/assets/footer/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import './Footer.css';
 import { Link } from 'react-router-dom';
-import ChatButton from '../chat/ChatButton'; // Zorg ervoor dat dit pad correct is
+import ChatButton from '../chat/ChatButton.jsx'; // Zorg ervoor dat dit pad correct is
 
 const Footer = ({ onChatClick }) => {
     return (
