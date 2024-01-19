5acd73e8efdadd2b9ce18e0b8257aacfb1d96c83
BramS21113653 <21113653@student.hhs.nl>
2024-01-19 12:20:24 +0100

    registreren user kiezen en redirect login

--- backend/Model/RegisterRequest.cs
+++ backend/Model/RegisterRequest.cs
@@ -1,8 +1,11 @@
-public class RegisterRequest
-{
+public class RegisterRequest {
     public string Username { get; set; }
     public string Email { get; set; }
     public string Password { get; set; }
-    public string FirstName { get; set; } 
-    public string LastName { get; set; } 
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserType { get; set; }
+    public string? CompanyName { get; set; } // Nullable CompanyName
+
+    // Other properties...
 }
